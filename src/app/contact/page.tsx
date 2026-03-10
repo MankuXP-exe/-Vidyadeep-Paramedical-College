e10063450177634b5349d2b7e6cb35fec236c284
Manku <ffarmylovers@gmail.com>
2026-03-10 13:33:09 +0530

    Fix Vercel build errors: Final iframe property types and build trigger

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -165,7 +165,7 @@ export default function ContactPage() {
                 </div>
             </Section>
 
-            {/* Map Section */}
+            {/* Campus Location Map */}
             <section className="h-[500px] w-full relative grayscale hover:grayscale-0 transition-all duration-700">
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3511.2343248881335!2d76.814234!3d28.445353999999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjjCsDI2JzQzLjMiTiA3Niw0OCc1MS4yIkU!5e0!3m2!1sen!2sin!4v1715424853245!5m2!1sen!2sin"
